77c397d7399c2d502a4c46c6d2302ddd628470c7
vulturepremier <vulturepremier@gmail.com>
2024-02-29 15:05:17 +0200

    My first commit

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * @enpv: environment vars
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv, char **enpv)
+{
+	interactive(enpv);
+	return (0);
+}
