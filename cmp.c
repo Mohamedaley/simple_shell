3944aff8f7cf3c52dfea5fc4f1ed18d045eb4dc6
vulturepremier <vulturepremier@gmail.com>
2024-03-01 19:47:50 +0200

    The ALX simple shell Project

--- cmp.c
+++ cmp.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- *
+ * cmp - A function to exit
+ * @str: the string which can include exit statement
+ * Return: (1) on Success (0) on failure;
  */
 int cmp(char *str)
 {
-    char *ext = "exit";
+	char *ext = "exit";
 
-    if (strcmp(str, ext) == 0)
-    {
-        return (1);
-    }
-    else
-        return (0);
+	if (strcmp(str, ext) == 0)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
